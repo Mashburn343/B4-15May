fcbbb66b7c429b5d8f43b457f213f91c5b23c711
Mashburn343 <taylor.mashburn.343@gmail.com>
2017-05-15 14:12:25 -0600

    I Hope it works

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -4,6 +4,7 @@ public class GitRunner {
 	public static void main(String[] args){
 		System.out.println("Initial Build - new project, yeah!");
 		System.out.println("Start new, risky GUI");
+		System.out.println("Please work");
 		// this is a practice for commit to get a new version
 	}
 
