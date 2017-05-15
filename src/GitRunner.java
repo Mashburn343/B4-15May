dc2cc7859ab1a779501c851eae7d1bd972db2b7d
Mashburn343 <taylor.mashburn.343@gmail.com>
2017-05-15 14:14:32 -0600

    Attempt number 2 :)

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -3,6 +3,8 @@ public class GitRunner {
 	
 	public static void main(String[] args){
 		System.out.println("Initial Build - new project, yeah!");
+		
+		System.out.println("Bug fix for null");
 	}
 
 }
