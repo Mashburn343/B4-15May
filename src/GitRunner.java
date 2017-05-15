f8b0a5b754415ddc0da13dc9c1434f400d66399e
Mashburn343 <taylor.mashburn.343@gmail.com>
2017-05-15 14:10:04 -0600

    Start Risky GUI

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -3,6 +3,8 @@ public class GitRunner {
 	
 	public static void main(String[] args){
 		System.out.println("Initial Build - new project, yeah!");
+		System.out.println("Start new, risky GUI");
+		// this is a practice for commit to get a new version
 	}
 
 }
